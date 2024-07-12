d18bb9ff1ea4d6b357bc58d2ddc58ded2b54bd37
Gangadharam Vaddi <117144674+gangadharpr@users.noreply.github.com>
2024-07-13 01:22:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-// src/App.js
+    import React from 'react';
+    import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+    import Navbar from './Navbar/Navbar';
+    import Home from './Home/Home';
+    import Portfolio from './Portfolio/Portfolio';
+    import Admin from './Admin/Admin';
+    import Clients from './Clients/Clients';
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
-import Home from './Home/Home';
-import Portfolio from './Portfolio/Portfolio';
-import Admin from './Admin/Admin';
-import Clients from './Clients/Clients';
+    const App = () => {
+        return (
+            <div>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/Home" element={<Home />} />
+                        <Route path="/Portfolio" element={<Portfolio />} />
+                        <Route path="/Admin" element={<Admin />} />
+                        <Route path="/Clients" element={<Clients />} />
+                    </Routes>
+                </Router>
+            </div>
+        );
+    };
 
-const App = () => {
-    return (
-        <div>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/Portfolio" element={<Portfolio />} />
-                    <Route path="/Admin" element={<Admin />} />
-                    <Route path="/Clients" element={<Clients />} />
-                </Routes>
-            </Router>
-        </div>
-    );
-};
-
-export default App;
+    export default App;
